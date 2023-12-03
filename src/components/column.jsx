a0ddc5b944b31bfb53809d4852f3c2378c7674f4
mikeworldyt <diazmig98@gmail.com>
2023-12-03 14:10:10 -0600

    Update

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -1,21 +1,37 @@
 import './styles/Components.css'
-
+import React, { useState } from 'react';
 
 function Container(props) {
+  const levelsData = {
+    level1: ['Tienda Roja', 'Tienda Azul', 'Tienda Verde'],
+    level2: ['pinta la', 'segunda columna'],
+    level3: ['pinta la', 'tercera columna'],
+  //  level4: ['pinta la', 'cuarta columna'],
+  };
+
+  const data = levelsData[`level${props.id}`] || [];
+  const hasData = data.length > 0;
+
   return (
-      <container id='' className='container '>
-        <div className='col-h'>
-          <h4> Level {props.id}</h4>
-        </div>
-        <ul>
-          <li><button>element</button></li>
-          <li><button>element</button></li>
-          <li><button>element</button></li>
-          <li><button>element</button></li>
-          <li><button>element</button></li>
-        </ul>
-      </container>
-  )
+    <>
+      {hasData && (
+        <container className='level' id={`level${props.id}`}>
+          <div className='col-h'>
+            <h4> Level {props.id}</h4>
+          </div>
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>
+                <button>
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </container>
+      )}
+    </>
+  );
 }
 
 export default Container
