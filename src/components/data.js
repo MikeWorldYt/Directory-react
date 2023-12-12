cc5eb3423437a33ce4716c01c50ecdf9dfaf232e
mikeworldyt <diazmig98@gmail.com>
2023-12-11 22:26:30 -0600

    data fill

--- src/components/data.js
+++ src/components/data.js
@@ -2,10 +2,18 @@
 const levelsData = {
   alldata: [
     { id: '100' , level: 1, label: 'Personal'},
+
     { id: '200' , level: 1, label: 'Work' },
+   //   { id: '210' , level: 1, label: 'Photography' },
+   //   { id: '220' , level: 1, label: 'Frontend' },
+
     { id: '300' , level: 1, label: 'Projects' },
+
     { id: '400' , level: 1, label: 'College'},
-      { id: '410' , level: 2, label: '(lvl 2) Math'}, //
+      { id: '410' , level: 2, label: '(lvl 2) Lenguage'},
+    //  { id: '420' , level: 2, label: '(lvl 2) ForeignLenguage'},
+    //  { id: '430' , level: 2, label: '(lvl 2) Math'},
+
     { id: '500' , level: 1, label: 'Hobbies'},
       { id: '510' , level: 2, label: '(lvl 2) Games'}, 
   ],
@@ -34,13 +42,17 @@ siguiendo esa logica seria:
 click en `Rojos`
 
 `<li id: '110' , level: '2' ><Rojos> </></>`
-FILTRO 1 = cual es el valor de la propiedad level =  `2` 
-entonces `2++` filtro los del  `level3`
+FILTRO 1 = 
+- toma el valor de la propiedad id =  `2` 
+- entonces `2++` y devuelve  `level3`
+  (levelsData[allData] || []).filter((obj) => obj.id === "200")
 
 FILTRO 2= cual es el id del click? `110` = 
 ok, entonces el dato numero de la posicion `2`  
 que es  `1`  y filtro aquellos id que tengan en la 
 posicion `2` el `1`
+
+  (levelsData[allData] || []).filter((obj) => obj.id.startsWith(4))
 
 RESULTADO: devuelvo un array nuevo con:
 ```
@@ -57,5 +69,39 @@ con  `el nuevo array`
 <li id: '112' , level: '3' ><Rubi> </></>
 <li id: '113' , level: '3' ><Carmesi> </></>
 ```
+
+
+
+function getNumberPosition(num, pos) {
+  const position = pos - 1
+  const number = `${num}`
+  const capture = number.codePointAt(`${position}`);
+
+  if (position.length >= 4) {
+      return `nothing`
+  } 
+    
+  `el numero de la posición ${pos} es: ${String.fromCodePoint(`${capture}`)}`
+
+
+}
+
+function getNumberPosition(num, pos) {
+  const position = pos - 1
+  const number = `${num}`
+  const capture = number.codePointAt(`${position}`);
+
+  if(number.length >= 3 ){
+  return `el numero de la posición ${pos} es: ${String.fromCodePoint(`${capture}`)}`
+  } 
+  else if(number.length <= 4 ) {
+    return `number.length <= 4`
+  }
+  
+  else {
+    return `parametros inavlidos`
+  }
+    return `fuera`
+}
 
 */
