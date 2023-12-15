72719a32c4313e166d52359c6f7c0f945d36ce7c
mikeworldyt <diazmig98@gmail.com>
2023-12-15 16:11:48 -0600

    To do comments

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -2,15 +2,13 @@ import PropTypes from 'prop-types';
 import './styles/Components.css';
 import levelsData from './data';
 
-
 export default function Container(props) {
-  const allData = 'alldata' // modificar para pintar
-  const data = levelsData[allData] || []; // modificar para pintar aparte
+  const allData = 'alldata' // TODO:             modificar para pintar
+  const data = levelsData[allData] || []; // TODO:    modificar para pintar aparte
   const hasData = data.length > 0;
   const filter1 = Number(`${props.id}`) + 1
 
   const handleClick = () => {
-      
       console.log(`click ${filter1} `);
       console.log('array')
     };
