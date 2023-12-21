d5515f034e25f384f6c341e7b47de530a84ffd55
mikeworldyt <diazmig98@gmail.com>
2023-12-20 20:23:47 -0600

    checkedTest

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
 import './styles/Components.css';
 import levelsData from './data';
+import { useState } from 'react';
 
 export default function Container(props) {
   const allData = 'alldata' // TODO:             modificar para pintar
   const data = levelsData[allData] || []; // TODO:    modificar para pintar aparte
   const hasData = data.length > 0;
-  const filter1 = Number(`${props.id}`) + 1
+  const filter1 = Number(`${props.id}`) + 1;
+  const [selectedItem, setSelectedItem] = useState(null);
 
   const handleClick = (dataLevel) => {
       console.log(`click `);
       console.log({dataLevel})
+      setSelectedItem(dataLevel);
     };
 
+    /*function LevelBuilder(v1, v2){
+      this.v1 = v1
+      this.v2 = v2
+    }
+  
+    let buildLevel = new LevelBuilder(1, 2)
+    */
+  
   return (
     <>
       {hasData && (
@@ -23,8 +34,20 @@ export default function Container(props) {
           <ul>
             {data.map((item, index) => (
               <li key={index}>
-                <label onClick={() => handleClick(item.data)} id={item.id} data-level={item.level}>
-                  <input name={props.id} type="radio"></input>{item.label}
+                <label
+                  onClick={() => handleClick(item.data)}
+                  id={item.id}
+                  data-level={item.level}
+                  
+                  >
+                  <input 
+                  name={props.id}
+                  type="radio"
+                  className={selectedItem === item ? 'checked' : ''}
+                  
+                  onChange={() => handleRadioChange(item)}
+                  >
+                  </input>{item.label}
                 </label>
               </li>
             ))}
