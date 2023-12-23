d32ec531a165ac33903293acd11401e113019654
mikeworldyt <diazmig98@gmail.com>
2023-12-22 19:23:01 -0600

    first commit

---
+++ src/components/column2.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import './styles/Components.css';
+import levelsData from './data';
+import { useState } from 'react';
+
+export default function Container2(props) {
+  const allData = 'alldata' // TODO:             modificar para pintar
+  const data =  levelsData.alldata[3].data || []; // TODO:    modificar para pintar aparte
+  const hasData = data.length > 0;
+  const filter1 = Number(`${props.id}`) + 1;
+  const [selectedItem, setSelectedItem] = useState(null);
+
+const handleClick = (dataLevel) => {
+      console.log(`click `);
+      ///console.log({dataLevel})
+      setSelectedItem(dataLevel);
+      
+    };
+
+    /*function LevelBuilder(v1, v2){
+      this.v1 = v1
+      this.v2 = v2
+    }
+  
+    let buildLevel = new LevelBuilder(1, 2)
+    */
+  
+  return (
+    <>
+      {hasData && (
+        <container className='level' id={`level${props.id}`}>
+          <div className='col-h'>
+            <h4> Level {props.id}</h4>
+          </div>
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>
+                <button onClick={() => handleClick(item.data)}
+                  id={item.id}
+                  data-level={item.level}
+                  >{item.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </container>
+      )}
+    </>
+  );
+}
+
+Container2.propTypes = {
+  id: PropTypes.string.isRequired,
+};
