87875b681fdb4d8744f83b5212246a639bae2e78
mikeworldyt <diazmig98@gmail.com>
2023-12-26 18:24:20 -0600

    DEBUG

--- src/components/column4.jsx
+++ src/components/column4.jsx
@@ -5,14 +5,16 @@ import levelsData from './data';
 
 export default function Container4(props) {
   const colA = props.colA
-  const data = levelsData.alldata[1].data[0].data[0].data || [] ///colD !== undefined ? ( levelsData.alldata[1].data[0].data[0].data || [] ) : []; // testing data
+  const colB = undefined
+  const colC = undefined
+  const data = levelsData.alldata[1].data[0].data[0].data || [] ///colD !== undefined ? ( levelsData.alldata[colA].data[0].data[0].data || [] ) : []; // testing data
   const hasData = data.length > 0;
 
   return (
     <>
       {hasData && (
         <div className='container level' > {/* id={`level${props.colC}`} */}
-          <div className='col-h'>
+            <div className='col-h'>
             <h4> Level 4 {/*{props.colC}*/}</h4>
           </div>
           <ul>
@@ -26,6 +28,10 @@ export default function Container4(props) {
               </li>
             ))}
           </ul>
+          <h1>DEBUG DATA:</h1>
+          <h1>colA - 1: {colA}</h1>
+          <h1>colB - 2: {colB}</h1>
+          <h1>colC - 3: {colC}</h1>
         </div>
       )}
     </>
