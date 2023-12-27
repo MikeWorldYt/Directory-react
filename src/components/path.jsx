5f3d5a73473b99824b61e73b92b0115d1545a669
mikeworldyt <diazmig98@gmail.com>
2023-12-27 15:23:18 -0600

    refactorized

--- src/components/path.jsx
+++ src/components/path.jsx
@@ -2,7 +2,9 @@ import './styles/Components.css'
 
 function Path() {
   return (
-    <h3 className='path'>Root/Level1/Level2/Level3/Level4</h3>
+    <>
+      <h3 className='path'>Root/Level1/Level2/Level3/Level4</h3>
+    </>
   )
 }
 
