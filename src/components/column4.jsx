feb506d6e037e5ee61a420c4f43c8eeb50b77f0f
mikeworldyt <diazmig98@gmail.com>
2023-12-26 18:19:48 -0600

    component content

--- src/components/column4.jsx
+++ src/components/column4.jsx
@@ -5,31 +5,31 @@ import levelsData from './data';
 
 export default function Container4(props) {
   const colA = props.colA
-    const data = levelsData.alldata[1].data[0].data[0].data || [] ///colD !== undefined ? ( levelsData.alldata[1].data[0].data[0].data || [] ) : []; // testing data
-    const hasData = data.length > 0;
+  const data = levelsData.alldata[1].data[0].data[0].data || [] ///colD !== undefined ? ( levelsData.alldata[1].data[0].data[0].data || [] ) : []; // testing data
+  const hasData = data.length > 0;
 
-    return (
-      <>
-        {hasData && (
-          <div className='container level' > {/* id={`level${props.colC}`} */}
-            <div className='col-h'>
-              <h4> Level 4 {/*{props.colC}*/}</h4>
-            </div>
-            <ul>
-              {data.map((item, index) => (
-                <li key={index}>
-                  <button onClick={() => handleClick(item.id, item.level, item.label)}
-                    id={item.id}
-                    data-level={item.level}
-                    >{item.label}
-                  </button>
-                </li>
-              ))}
-            </ul>
+  return (
+    <>
+      {hasData && (
+      <div className='col-h'>
+        <div className='container level' > {/* id={`level${props.colC}`} */}
+            <h4> Level 4 {/*{props.colC}*/}</h4>
           </div>
-        )}
-      </>
-    );
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>
+                <button onClick={() => handleClick(item.id, item.level, item.label)}
+                  id={item.id}
+                  data-level={item.level}
+                  >{item.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 Container4.propTypes = {
