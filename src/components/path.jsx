78b89af7f713baf49e9edca255f42e5e702bdaf9
mikeworldyt <diazmig98@gmail.com>
2023-12-28 14:56:21 -0600

    useContext

--- src/components/path.jsx
+++ src/components/path.jsx
@@ -1,11 +1,12 @@
 import './styles/Components.css'
 import { DirContext } from '../context/directory'
+import { useContext } from 'react'
 export default function Path( ) {
-  
+  const { pathA } = useContext( DirContext );
 
   return (
     <>
-      <h3 className='path'>Root/</h3>
+      <h3 className='path'>Root:/{pathA}/</h3>
     </>
   )
 }
