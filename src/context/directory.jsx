2e996206d23edb98865a372b48336ca13618c74e
mikeworldyt <diazmig98@gmail.com>
2023-12-28 13:43:43 -0600

    useContext

--- src/context/directory.jsx
+++ src/context/directory.jsx
@@ -1,21 +1,18 @@
-import PropTypes from 'prop-types';
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // crear contexto
 export const DirContext = createContext()
 
 // crear provider
 export function DirProvider( { children }) {
+    const [text, setText] = useState('')
 
     return (
         <DirContext.Provider value={{
-
+            text, 
+            setText
         }}>
             {children}
         </DirContext.Provider>
     )
-}
-
-DirProvider.propTypes = {
-    children: PropTypes.node
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { DirProvider } from './context/directory'
 
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <DirProvider>
     <App />
-  </React.StrictMode>,
+  </DirProvider>,
 )
