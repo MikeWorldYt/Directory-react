f2b23121c29eb79a78a79cca2b13e9323987d226
mikeworldyt <diazmig98@gmail.com>
2024-01-08 18:30:04 -0600

    first importation

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,21 @@
+import iconApp from './iconApp.svg';
+import iconAdd from './iconAdd.svg';
+import iconDivider from './iconDivider.svg';
+import folderEmerald from './iconFolderEmerald.svg';
+import folderBlue from './iconFolderBlue.svg';
+import folderRed from './iconFolderRed.svg';
+import folderYellow from './iconFolderYellow.svg';
+import folderOrange from './iconFolderOrange.svg';
+import folderBrown from './iconFolderBrown.svg';
+
+export default {
+    'app' : iconApp,
+    'add' : iconAdd,
+    'div' : iconDivider,
+    'folderEmerald' : folderEmerald,
+    'folderBlue' : folderBlue,
+    'folderRed' : folderRed,
+    'folderYellow' : folderYellow,
+    'folderOrange' : folderOrange,
+    'folderBrown' : folderBrown
+}
