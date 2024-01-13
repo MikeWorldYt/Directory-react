ad524be319fb17f37f86de056b6dd5846c503d83
mikeworldyt <diazmig98@gmail.com>
2024-01-13 13:05:23 -0600

    chevron element

--- src/components/win__workspace/1__path/path.jsx
+++ src/components/win__workspace/1__path/path.jsx
@@ -1,14 +1,23 @@
 import { useContext } from 'react'
 import { DirContext } from '../../../context/directory'
 import '../../styles/Components.css'
+import icon from '../../../assets/icons/icons.js';
+
+const Chevron = (
+  <img className='iconSmall' src={icon.chRight} />
+);
 
 export default function Head() {
   const { pathA, pathB, pathC } = useContext( DirContext );
-
   return (
     <section id='head'>
       <div className='container'>
-        <h4 className='path'>Root:/{pathA}{pathB}{pathC}</h4>
+        <h4 className='path'>Root
+        {Chevron}
+        {pathA}
+        {pathB}
+        {pathC}
+        </h4>
       </div>
     </section>
   )
